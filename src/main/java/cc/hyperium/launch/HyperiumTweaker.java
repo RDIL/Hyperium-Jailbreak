170c007ba93a04ec1539e4696f285c225933b087
Reece Dunham <me@rdil.rocks>
2019-04-05 16:53:00 -0400

    move back

--- src/main/java/cc/hyperium/launch/HyperiumTweaker.java
+++ src/main/java/cc/hyperium/launch/HyperiumTweaker.java
@@ -14,7 +14,7 @@
  *     You should have received a copy of the GNU Lesser General Public License
  *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package rocks.rdil.jailbreak;
+package cc.hyperium.launch;
 import cc.hyperium.Hyperium;
 import cc.hyperium.internal.addons.AddonBootstrap;
 import net.minecraft.launchwrapper.ITweaker;
