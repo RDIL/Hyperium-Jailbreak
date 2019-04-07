8101871f7929a92d0f3b27987563a50bb2540046
Reece Dunham <me@rdil.rocks>
2019-04-07 12:08:21 -0400

    Update HyperiumMinecraft.java

--- src/main/java/cc/hyperium/mixinsimp/HyperiumMinecraft.java
+++ src/main/java/cc/hyperium/mixinsimp/HyperiumMinecraft.java
@@ -46,7 +46,6 @@ import net.minecraft.profiler.Profiler;
 import net.minecraft.util.Timer;
 import net.minecraft.util.Util;
 import net.minecraft.world.WorldSettings;
-import net.montoyo.mcef.MCEF;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
