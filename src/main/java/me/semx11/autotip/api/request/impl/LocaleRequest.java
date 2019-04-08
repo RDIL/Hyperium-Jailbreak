6469576e1f9925149e9b31a84dbe11e8b7851728
Reece Dunham <me@rdil.rocks>
2019-04-08 12:40:30 -0400

    Update LocaleRequest.java

--- src/main/java/me/semx11/autotip/api/request/impl/LocaleRequest.java
+++ src/main/java/me/semx11/autotip/api/request/impl/LocaleRequest.java
@@ -26,7 +26,7 @@ public class LocaleRequest implements Request<LocaleReply> {
     public LocaleReply execute() {
         HttpUriRequest request = GetBuilder.of(this)
                 .addParameter("lang", this.locale.toLanguageTag())
-                .addParameter("version", "v3.0")
+                .addParameter("v", "v3.0")
                 .build();
 
         Optional<Reply> optional = RequestHandler.getReply(this, request.getURI());
