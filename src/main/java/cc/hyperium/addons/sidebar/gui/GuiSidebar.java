b1c8d7c81f8ed90c0ccdc8053bc8a3716b8f1cbd
Reece Dunham <me@rdil.rocks>
2019-04-21 09:46:08 -0400

    Fix

--- src/main/java/cc/hyperium/addons/sidebar/gui/GuiSidebar.java
+++ src/main/java/cc/hyperium/addons/sidebar/gui/GuiSidebar.java
@@ -21,6 +21,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.scoreboard.ScoreObjective;
 import net.minecraft.util.EnumChatFormatting;
 import org.lwjgl.opengl.GL11;
 import java.awt.*;
