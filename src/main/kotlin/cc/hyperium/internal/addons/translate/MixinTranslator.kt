cc4b239391f38f3ef438c59a32734b98ea26fe2a
Reece Dunham <me@rdil.rocks>
2019-04-23 09:17:25 -0400

    Update MixinTranslator.kt

--- src/main/kotlin/cc/hyperium/internal/addons/translate/MixinTranslator.kt
+++ src/main/kotlin/cc/hyperium/internal/addons/translate/MixinTranslator.kt
@@ -1,6 +1,7 @@
 package cc.hyperium.internal.addons.translate
 
 import cc.hyperium.internal.addons.AddonManifest
+import cc.hyperium.internal.addons.AbstractTranslator
 import org.spongepowered.asm.mixin.Mixins
 
 class MixinTranslator : AbstractTranslator() {
