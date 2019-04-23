4b473077c8ce9141f6776b6c2960c9e148c30919
Reece Dunham <me@rdil.rocks>
2019-04-23 09:14:58 -0400

    Update and rename src/main/kotlin/cc/hyperium/internal/addons/translate/InstanceTranslator.kt to src/main/java/cc/hyperium/internal/addons/InstanceTranslator.java

---
+++ src/main/java/cc/hyperium/internal/addons/InstanceTranslator.java
@@ -0,0 +1,9 @@
+package cc.hyperium.internal.addons.translate
+
+import cc.hyperium.internal.addons.AddonManifest
+import cc.hyperium.internal.addons.AbstractTranslator
+
+public class InstanceTranslator extends AbstractTranslator {
+    @Override
+    public void translate(AddonManifest m) {}
+}
