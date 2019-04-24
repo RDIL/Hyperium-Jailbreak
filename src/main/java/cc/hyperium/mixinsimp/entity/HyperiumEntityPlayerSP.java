6e9ab7b173a01305576889d3a438a0dfe5514e3e
Reece Dunham <me@rdil.rocks>
2019-04-24 10:32:35 -0400

    Update HyperiumEntityPlayerSP.java

--- src/main/java/cc/hyperium/mixinsimp/entity/HyperiumEntityPlayerSP.java
+++ src/main/java/cc/hyperium/mixinsimp/entity/HyperiumEntityPlayerSP.java
@@ -7,11 +7,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.EnumParticleTypes;
 
 public class HyperiumEntityPlayerSP {
-    private EntityPlayerSP parent;
-
-    public HyperiumEntityPlayerSP(EntityPlayerSP parent) {
-        this.parent = parent;
-    }
+    public HyperiumEntitiyPlayerSP() {}
 
     public void onEnchantmentCritical(Entity entityHit, Minecraft mc) {
         if (mc.isSingleplayer() || !Settings.CRIT_FIX) {
@@ -20,7 +16,7 @@ public class HyperiumEntityPlayerSP {
     }
 
     public void onCriticalHit(Entity entityHit, Minecraft mc) {
-        if (Minecraft.getMinecraft().isSingleplayer() || !Settings.CRIT_FIX) {
+        if (mc.isSingleplayer() || !Settings.CRIT_FIX) {
             mc.effectRenderer.emitParticleAtEntity(entityHit, EnumParticleTypes.CRIT);
         }
     }
