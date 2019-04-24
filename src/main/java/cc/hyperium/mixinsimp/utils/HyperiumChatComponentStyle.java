779ee5c347594bf36fd150ad73d69e77d2fffd21
Reece Dunham <me@rdil.rocks>
2019-04-24 10:22:08 -0400

    Cleanup mixins

--- src/main/java/cc/hyperium/mixinsimp/utils/HyperiumChatComponentStyle.java
+++ src/main/java/cc/hyperium/mixinsimp/utils/HyperiumChatComponentStyle.java
@@ -1,24 +1,16 @@
 package cc.hyperium.mixinsimp.utils;
-
-import net.minecraft.util.ChatComponentStyle;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 public class HyperiumChatComponentStyle {
-
-    private ChatComponentStyle parent;
     private String cache;
-
-    public HyperiumChatComponentStyle(ChatComponentStyle parent) {
-        this.parent = parent;
-    }
+    public HyperiumChatComponentStyle() {}
 
     public void invalidateCache() {
         this.cache = null;
     }
 
     public void getFormatedTextHeader(CallbackInfoReturnable<String> string) {
-        if (cache != null)
-            string.setReturnValue(cache);
+        if (cache != null) string.setReturnValue(cache);
     }
 
     public void getFormatedTextReturn(CallbackInfoReturnable<String> string) {
