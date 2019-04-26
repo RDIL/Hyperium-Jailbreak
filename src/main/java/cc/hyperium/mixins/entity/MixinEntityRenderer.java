c07f7a6c85b22c50cef18cb6db4aa2b7be187c77
Reece Dunham <me@rdil.rocks>
2019-04-26 08:51:34 -0400

    Update MixinEntityRenderer.java

--- src/main/java/cc/hyperium/mixins/entity/MixinEntityRenderer.java
+++ src/main/java/cc/hyperium/mixins/entity/MixinEntityRenderer.java
@@ -31,7 +31,6 @@ import net.minecraft.util.EntitySelectors;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItemFrame;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
