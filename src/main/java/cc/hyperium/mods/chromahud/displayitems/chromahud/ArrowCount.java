3382b0449aa6a37193b0be34c27e745a3a8e7917
Reece Dunham <me@rdil.rocks>
2019-04-27 18:57:59 -0400

    cleanup

--- src/main/java/cc/hyperium/mods/chromahud/displayitems/chromahud/ArrowCount.java
+++ src/main/java/cc/hyperium/mods/chromahud/displayitems/chromahud/ArrowCount.java
@@ -17,7 +17,6 @@
 
 package cc.hyperium.mods.chromahud.displayitems.chromahud;
 
-
 import cc.hyperium.mods.chromahud.ElementRenderer;
 import cc.hyperium.mods.chromahud.api.DisplayItem;
 import cc.hyperium.utils.JsonHolder;
@@ -25,21 +24,15 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Sk1er
- */
 public class ArrowCount extends DisplayItem {
-
     public ArrowCount(JsonHolder data, int ordinal) {
         super(data, ordinal);
         this.height = 16;
         this.width = 16;
     }
-
 
     @Override
     public void draw(int starX, double startY, boolean isConfig) {
@@ -53,11 +46,9 @@ public class ArrowCount extends DisplayItem {
                     if (is.getUnlocalizedName().equalsIgnoreCase("item.arrow"))
                         c += is.stackSize;
                 }
-
             }
             ElementRenderer.render(list, starX, startY, false);
             ElementRenderer.draw(starX + 16, startY + 8, "x" + (isConfig ? 64 : c));
         }
     }
-
 }
