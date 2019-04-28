f8ec28b74716c5f1f138cdd2538cbfdb623f3b5a
Reece Dunham <me@rdil.rocks>
2019-04-28 14:27:10 -0400

    Cleanup

--- src/main/java/cc/hyperium/mods/tabtoggle/TabToggleMod.java
+++ src/main/java/cc/hyperium/mods/tabtoggle/TabToggleMod.java
@@ -5,11 +5,9 @@ import cc.hyperium.event.EventBus;
 import cc.hyperium.mods.AbstractMod;
 
 public class TabToggleMod extends AbstractMod {
-
     @Override
     public AbstractMod init() {
         EventBus.INSTANCE.register(new TabToggleEventListener());
-
         TabToggleSettings object = new TabToggleSettings();
         Hyperium.CONFIG.register(object);
         Hyperium.INSTANCE.getHandlers().getSettingsHandler().getSettingsObjects().add(object);
