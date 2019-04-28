4bc62a1e98f81082c4d0dc30ddc281b6836c470d
Reece Dunham <me@rdil.rocks>
2019-04-28 18:37:08 -0400

    Remove unused imports

--- src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiScreenResourcePacks.java
+++ src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiScreenResourcePacks.java
@@ -6,10 +6,8 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiResourcePackAvailable;
 import net.minecraft.client.gui.GuiResourcePackSelected;
 import net.minecraft.client.gui.GuiScreenResourcePacks;
-import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.resources.ResourcePackListEntry;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class HyperiumGuiScreenResourcePacks {
