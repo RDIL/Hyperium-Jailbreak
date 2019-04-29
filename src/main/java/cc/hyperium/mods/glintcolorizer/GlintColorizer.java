6559a41f88a211d4d07e9cbf677696110a29de10
Reece Dunham <me@rdil.rocks>
2019-04-29 10:17:38 -0400

    Update GlintColorizer.java

--- src/main/java/cc/hyperium/mods/glintcolorizer/GlintColorizer.java
+++ src/main/java/cc/hyperium/mods/glintcolorizer/GlintColorizer.java
@@ -1,6 +1,7 @@
 package cc.hyperium.mods.glintcolorizer;
 
 import cc.hyperium.mods.glintcolorizer.Colors;
+import cc.hyperium.Hyperium;
 import cc.hyperium.mods.AbstractMod;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.InvokeEvent;
