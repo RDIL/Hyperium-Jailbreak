e69636aa343ca41af8d5c6a7189017bfb5a6e59b
Reece Dunham <me@rdil.rocks>
2019-04-30 12:42:18 -0400

    cleanup

--- src/main/java/cc/hyperium/mods/timechanger/commands/CommandTimeChangerFastTime.java
+++ src/main/java/cc/hyperium/mods/timechanger/commands/CommandTimeChangerFastTime.java
@@ -27,7 +27,6 @@ import cc.hyperium.utils.ChatColor;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class CommandTimeChangerFastTime implements BaseCommand {
-
     private final TimeChanger mod;
 
     public CommandTimeChangerFastTime(TimeChanger main) {
@@ -46,19 +45,17 @@ public class CommandTimeChangerFastTime implements BaseCommand {
 
     @Override
     public void onExecute(String[] args) throws CommandException {
-        if (args.length == 0) {
-            throw new CommandUsageException();
-        }
+        if (args.length == 0) throw new CommandUsageException();
 
         final double multiplier = NumberUtils.toDouble(args[0], -1.0);
 
         if (multiplier < 0.0) {
-            GeneralChatHandler.instance().sendMessage(ChatColor.RED + "[TimeChanger] " + ChatColor.RED + "Invalid multiplier!", false);
+            GeneralChatHandler.instance().sendMessage(ChatColor.RED + "Invalid multiplier!", false);
             return;
         }
 
         this.mod.setTimeType(TimeType.FAST);
         this.mod.setFastTimeMultiplier(multiplier);
-        GeneralChatHandler.instance().sendMessage(ChatColor.RED + "[TimeChanger] " + ChatColor.GREEN + "Time set to fast (" + multiplier + ").", false);
+        GeneralChatHandler.instance().sendMessage(ChatColor.GREEN + "Time set to fast (" + multiplier + ").", false);
     }
 }
