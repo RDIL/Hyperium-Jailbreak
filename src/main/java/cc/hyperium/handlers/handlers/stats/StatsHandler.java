72c71b5168257377207f6338bdfb20a8c034af7b
Reece Dunham <me@rdil.rocks>
2019-05-02 14:54:33 -0400

    Update StatsHandler.java

--- src/main/java/cc/hyperium/handlers/handlers/stats/StatsHandler.java
+++ src/main/java/cc/hyperium/handlers/handlers/stats/StatsHandler.java
@@ -2,6 +2,7 @@ package cc.hyperium.handlers.handlers.stats;
 
 import cc.hyperium.handlers.handlers.data.HypixelAPI;
 import cc.hyperium.mods.sk1ercommon.Multithreading;
+import club.sk1er.website.api.requests.HypixelApiGuild;
 import club.sk1er.website.api.requests.HypixelApiPlayer;
 
 public class StatsHandler {
@@ -11,6 +12,27 @@ public class StatsHandler {
                 HypixelApiPlayer apiPlayer = HypixelAPI.INSTANCE.getPlayer(player).get();
 
                 new PlayerStatsGui(apiPlayer).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public void loadGuildByPlayer(String player) {
+        Multithreading.runAsync(() -> {
+            try {
+                HypixelApiGuild apiGuild = HypixelAPI.INSTANCE.getGuildFromPlayer(player).get();
+
+                new GuildStatsGui(apiGuild).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+    public void loadGuildByName(String name) {
+        Multithreading.runAsync(() -> {
+            try {
+                HypixelApiGuild apiGuild = HypixelAPI.INSTANCE.getGuildFromName(name).get();
+                new GuildStatsGui(apiGuild).show();
             } catch (Exception e) {
                 e.printStackTrace();
             }
