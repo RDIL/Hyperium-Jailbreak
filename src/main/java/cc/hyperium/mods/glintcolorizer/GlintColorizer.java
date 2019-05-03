23e20667ed170e75340eac9833ef89994153969a
Reece Dunham <me@rdil.rocks>
2019-05-03 15:16:41 -0400

    Update GlintColorizer.java

--- src/main/java/cc/hyperium/mods/glintcolorizer/GlintColorizer.java
+++ src/main/java/cc/hyperium/mods/glintcolorizer/GlintColorizer.java
@@ -4,16 +4,13 @@ import cc.hyperium.config.Settings;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.event.TickEvent;
-import java.awt.Color;
+import cc.hyperium.mods.glintcolorizer.Colors;
 
 public class GlintColorizer extends AbstractMod {
-    private static float[] onepoint8glintcolorF = Color.RGBtoHSB(Settings.glintR, Settings.glintG, Settings.glintB, null);
-    public static int onepoint8glintcolorI = Color.HSBtoRGB(onepoint8glintcolorF[0], onepoint8glintcolorF[1], onepoint8glintcolorF[2]);
-
     @Override
     public AbstractMod init() {
         EventBus.INSTANCE.register(this);
-        setonepoint8color(Settings.glintR, Settings.glintG, Settings.glintB);
+        Colors.setonepoint8color(Colors.glintR, Colors.glintG, Colors.glintB);
         return this;
     }
 
@@ -21,40 +18,18 @@ public class GlintColorizer extends AbstractMod {
     @InvokeEvent
     public void onTick(TickEvent e) {
         if (!Settings.glintColorizer) {
-            if (onepoint8glintcolorI != -8372020) onepoint8glintcolorI = -8372020;
+            if (Colors.onepoint8glintcolorI != -8372020) Colors.onepoint8glintcolorI = -8372020;
             return;
         }
-        if (Settings.glintcolorChroma) {
-            onepoint8glintcolorI = Color.HSBtoRGB(System.currentTimeMillis() % 10000L / 10000.0f, 0.8f, 0.8f);
+        if (Colors.glintcolorChroma) {
+            Colors.onepoint8glintcolorI = Color.HSBtoRGB(System.currentTimeMillis() % 10000L / 10000.0f, 0.8f, 0.8f);
             return;
         }
-        onepoint8glintcolorI = getIntFromColor(Settings.glintR, Settings.glintG, Settings.glintB);
+        Colors.onepoint8glintcolorI = getIntFromColor(Colors.glintR, Colors.glintG, Colors.glintB);
     }
 
     @Override
     public Metadata getModMetadata() {
         return new Metadata(this, "GlintColorizer", "1", "powns");
-    }
-
-    private int getIntFromColor(int red, int green, int blue) {
-        red = (red << 16) & 0x00FF0000;
-        green = (green << 8) & 0x0000FF00;
-        blue = blue & 0x000000FF;
-        return 0xFF000000 | red | green | blue;
-    }
-
-    public static void setonepoint8color(int r, int g, int b) {
-        Settings.glintR = r;
-        Settings.glintG = g;
-        Settings.glintB = b;
-        onepoint8glintcolorF = Color.RGBtoHSB(Settings.glintR, Settings.glintG, Settings.glintB, null);
-        onepoint8glintcolorI = Color.HSBtoRGB(onepoint8glintcolorF[0], onepoint8glintcolorF[1], onepoint8glintcolorF[2]);
-    }
-
-    public void setChroma(boolean bool) {
-        if (!(Settings.glintcolorChroma = bool)) {
-            onepoint8glintcolorF = Color.RGBtoHSB(Settings.glintR, Settings.glintG, Settings.glintB, null);
-            onepoint8glintcolorI = Color.HSBtoRGB(onepoint8glintcolorF[0], onepoint8glintcolorF[1], onepoint8glintcolorF[2]);
-        }
     }
 }
