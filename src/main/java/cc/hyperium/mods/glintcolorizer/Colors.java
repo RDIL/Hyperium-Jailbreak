5be2b5756ab179005cdcc0c812ba9da1afa32d48
Reece Dunham <me@rdil.rocks>
2019-05-03 16:45:10 -0400

    Update Colors.java

--- src/main/java/cc/hyperium/mods/glintcolorizer/Colors.java
+++ src/main/java/cc/hyperium/mods/glintcolorizer/Colors.java
@@ -44,11 +44,4 @@ public class Colors {
             onepoint8glintcolorI = Color.HSBtoRGB(onepoint8glintcolorF[0], onepoint8glintcolorF[1], onepoint8glintcolorF[2]);
         }
     }
-
-    public int getIntFromColor(int red, int green, int blue) {
-        red = (red << 16) & 0x00FF0000;
-        green = (green << 8) & 0x0000FF00;
-        blue = blue & 0x000000FF;
-        return 0xFF000000 | red | green | blue;
-    }
 }
