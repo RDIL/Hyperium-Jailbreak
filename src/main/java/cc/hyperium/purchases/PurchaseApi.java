a4f106b13c422dabb2c326e80f8379a7971d1112
Reece Dunham <me@rdil.rocks>
2019-05-12 16:48:54 -0400

    fix

--- src/main/java/cc/hyperium/purchases/PurchaseApi.java
+++ src/main/java/cc/hyperium/purchases/PurchaseApi.java
@@ -166,5 +166,6 @@ public class PurchaseApi {
         HyperiumPurchase value = new HyperiumPurchase(uuid, get(url + uuid.toString()));
         EventBus.INSTANCE.post(new PurchaseLoadEvent(uuid, value, true));
         purchasePlayers.put(uuid, value);
+        Hyperium.INSTANCE.getHandlers().getCapeHandler().deleteCape(uuid);
     }
 }
