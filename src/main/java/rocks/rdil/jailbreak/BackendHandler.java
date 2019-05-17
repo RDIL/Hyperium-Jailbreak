50c6b0eb1318d0384991d77d6d128f36feeb76c4
Reece Dunham <me@rdil.rocks>
2019-05-17 19:58:28 -0400

    K

--- src/main/java/rocks/rdil/jailbreak/BackendHandler.java
+++ src/main/java/rocks/rdil/jailbreak/BackendHandler.java
@@ -9,35 +9,19 @@ import cc.hyperium.installer.utils.http.impl.client.HttpClients;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import jb.Metadata;
 
 public class BackendHandler {
     private boolean update = false;
 
-    public BackendHandler() {}
-    
-    public void apiJoinRequest() {
-        try {
-            HttpClient httpclient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost("http://backend.rdil.rocks/join");
-
-            List<NameValuePair> params = new ArrayList<NameValuePair>(0);
-            try {
-                httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-            // Execute and get the response.
-            httpclient.execute(httppost);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public BackendHandler() {
+        EventBus.INSTANCE.register(this);
     }
 
-    public void apiLeaveRequest() {
+    public void apiRequest(String url) {
         try {
             HttpClient httpclient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost("http://backend.rdil.rocks/leave");
+            HttpPost httppost = new HttpPost("http://backend.rdil.rocks/" + url);
 
             // Request parameters and other properties.
             List<NameValuePair> params = new ArrayList<NameValuePair>(0);
@@ -67,22 +51,37 @@ public class BackendHandler {
 
             // Execute and get the response.
             String response = EntityUtils.toString(httpclient.execute(httppost).getEntity(), "UTF-8");
-            //System.out.println(response);
-            if (!response.equals(jb.Metadata.getVersion())){
-                //System.out.println("HITITITTI");
-                update = true;}
-            else{
+            if (!response.equals(Metadata.getVersion())) {
+                update = true;
+            } else {
                 update = false;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
     }
 
-    public boolean getUpdate(){
+    public boolean getUpdate() {
         return update;
+    }
+
+    @InvokeEvent
+    public void worldSwap(ServerJoinEvent event) {
+        boolean update = this.bh.getUpdate();
+        Runnable wait = new Runnable() {
+            public void run(){
+                while (Minecraft.getMinecraft().thePlayer == null) {}
+                if (update) {
+                    try {
+                        Thread.sleep(250);
+                        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "A new update for HyperiumJailbreak is available at " + EnumChatFormatting.BLUE + "https://www.rdil.rocks/update"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.submit(wait);
     }
 }
