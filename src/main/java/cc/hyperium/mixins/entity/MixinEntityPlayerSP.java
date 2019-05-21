b59f017237da6f2b9228c182d717e8ec37fefea2
Reece Dunham <me@rdil.rocks>
2019-05-21 12:13:00 -0400

    fix

--- src/main/java/cc/hyperium/mixins/entity/MixinEntityPlayerSP.java
+++ src/main/java/cc/hyperium/mixins/entity/MixinEntityPlayerSP.java
@@ -31,6 +31,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.potion.Potion;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.At;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
