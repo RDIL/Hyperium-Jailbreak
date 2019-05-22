2c74ddbd07dd16fed28005dea287da3a29b74138
Reece Dunham <me@rdil.rocks>
2019-05-22 13:04:12 -0400

    Update HyperiumGuiIngame.java

--- src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiIngame.java
+++ src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiIngame.java
@@ -24,7 +24,7 @@ public class HyperiumGuiIngame {
         this.parent = parent;
     }
 
-    public void renderSelectedItem(ScaledResolution sr) {
+    public void renderSelectedItem() {
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     }
 
