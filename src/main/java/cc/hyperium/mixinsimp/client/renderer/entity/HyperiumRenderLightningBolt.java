967fe420b1a8b5ca5d69918a40d1b5490f89f8df
Reece Dunham <me@rdil.rocks>
2019-08-11 23:04:46 -0400

    Update HyperiumRenderLightningBolt.java

--- src/main/java/cc/hyperium/mixinsimp/client/renderer/entity/HyperiumRenderLightningBolt.java
+++ src/main/java/cc/hyperium/mixinsimp/client/renderer/entity/HyperiumRenderLightningBolt.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.effect.EntityLightningBolt;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class HyperiumRenderLightningBolt {
-
     public void doRender(EntityLightningBolt entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
         if (Settings.DISABLE_LIGHTNING) {
             ci.cancel();
