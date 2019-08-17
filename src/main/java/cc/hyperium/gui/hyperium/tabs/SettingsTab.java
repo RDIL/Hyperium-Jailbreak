b6edc9f6349cc4f80df0c476ed15e32eeabcbb36
Reece Dunham <me@rdil.rocks>
2019-08-16 23:53:31 -0400

    Fixes

--- src/main/java/cc/hyperium/gui/hyperium/tabs/SettingsTab.java
+++ src/main/java/cc/hyperium/gui/hyperium/tabs/SettingsTab.java
@@ -1,5 +1,8 @@
 package cc.hyperium.gui.hyperium.tabs;
 
+import cc.hyperium.addons.bossbar.gui.GuiBossbarSetting;
+import cc.hyperium.addons.customcrosshair.gui.GuiCustomCrosshairEditCrosshair;
+import cc.hyperium.addons.sidebar.gui.screen.GuiScreenSettings;
 import cc.hyperium.config.Category;
 import cc.hyperium.config.SelectorSetting;
 import cc.hyperium.config.SliderSetting;
@@ -13,6 +16,10 @@ import cc.hyperium.gui.hyperium.components.RGBComponent;
 import cc.hyperium.gui.hyperium.components.SelectorComponent;
 import cc.hyperium.gui.hyperium.components.SliderComponent;
 import cc.hyperium.gui.hyperium.components.ToggleComponent;
+import cc.hyperium.gui.keybinds.GuiKeybinds;
+import cc.hyperium.mods.chromahud.gui.GeneralConfigGui;
+import cc.hyperium.mods.keystrokes.screen.GuiScreenKeystrokes;
+import cc.hyperium.mods.togglechat.gui.ToggleChatMainGui;
 import net.minecraft.client.resources.I18n;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -61,6 +68,14 @@ public class SettingsTab extends AbstractTab {
                 }
             }
         }
+
+        apply(new LinkComponent(this, Collections.emptyList(), "Sidebar Mod", new GuiScreenSettings(Hyperium.INSTANCE.getInternalAddons().getSidebarAddon())), true, Category.OTHER, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "Keystrokes", new GuiScreenKeystrokes(Hyperium.INSTANCE.getModIntegration().getKeystrokesMod())), true, Category.OTHER, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "Togglechat", new ToggleChatMainGui(Hyperium.INSTANCE.getModIntegration().getToggleChat(), 0)), true, Category.OTHER, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "Keybinds", new GuiKeybinds()), false, Category.GENERAL, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "Bossbar Addon", new GuiBossbarSetting()), true, Category.OTHER, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "Custom Crosshair", new GuiCustomCrosshairEditCrosshair(Hyperium.INSTANCE.getInternalAddons().getCustomCrosshairAddon())), true, Category.OTHER, items);
+        apply(new LinkComponent(this, Collections.emptyList(), "ChromaHUD", new GeneralConfigGui(Hyperium.INSTANCE.getModIntegration().getChromaHUD())), true, Category.CHROMAHUD, items);
 
         for (RGBFieldSet rgbFieldSet : gui.getRgbFields()) {
             apply(new RGBComponent(this, rgbFieldSet), rgbFieldSet.isMods(), rgbFieldSet.getCategory(), items);
