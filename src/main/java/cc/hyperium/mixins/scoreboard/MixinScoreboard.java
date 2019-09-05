a5af5e9bb820cc6290f4cd12a3a3e0e15f48826c
Reece Dunham <me@rdil.rocks>
2019-09-05 08:58:11 -0400

    Correctly shadow method

--- src/main/java/cc/hyperium/mixins/scoreboard/MixinScoreboard.java
+++ src/main/java/cc/hyperium/mixins/scoreboard/MixinScoreboard.java
@@ -31,7 +31,7 @@ public abstract class MixinScoreboard {
 
     @Shadow @Final private Map<String, ScorePlayerTeam> teamMemberships;
 
-    @Shadow private void func_96513_c;
+    @Shadow private abstract void func_96513_c(ScorePlayerTeam team);
 
     @Overwrite
     public void removeTeam(ScorePlayerTeam team) {
