3b5885537d3557e30636fd3fa04b9b9f6877b8e7
Reece Dunham <me@rdil.rocks>
2019-09-07 12:36:25 -0400

    Remove thing

--- src/main/java/cc/hyperium/network/LoginReplyHandler.java
+++ src/main/java/cc/hyperium/network/LoginReplyHandler.java
@@ -5,22 +5,16 @@ import cc.hyperium.netty.NettyClient;
 import cc.hyperium.netty.packet.PacketHandler;
 import cc.hyperium.netty.packet.PacketType;
 import cc.hyperium.netty.packet.packets.clientbound.LoginReplyPacket;
-import cc.hyperium.netty.packet.packets.serverbound.ServerCrossDataPacket;
 import cc.hyperium.netty.packet.packets.serverbound.UpdateLocationPacket;
-import cc.hyperium.utils.JsonHolder;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ServerData;
 
 public class LoginReplyHandler implements PacketHandler<LoginReplyPacket> {
     @Override
     public void handle(LoginReplyPacket loginReplyPacket) {
-        ServerData currentServerData = Minecraft.getMinecraft().getCurrentServerData();
-        if (currentServerData != null) {
+        if (Minecraft.getMinecraft().getCurrentServerData() != null) {
             NettyClient client = NettyClient.getClient();
             if (client != null) {
                 client.write(UpdateLocationPacket.build("Other"));
-                if (Settings.SEND_SERVER)
-                    client.write(ServerCrossDataPacket.build(new JsonHolder().put("internal", true).put("server_update", currentServerData.serverIP)));
             }
         }
     }
