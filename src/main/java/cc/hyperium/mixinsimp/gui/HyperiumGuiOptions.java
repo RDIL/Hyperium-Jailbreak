8e75d8ac2bb079d3a39b4d06ed0f145dee5fc786
Reece Dunham <me@rdil.rocks>
2019-09-09 10:59:07 -0400

    Progress to removing snooper gui

--- src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiOptions.java
+++ src/main/java/cc/hyperium/mixinsimp/gui/HyperiumGuiOptions.java
@@ -17,6 +17,10 @@ public class HyperiumGuiOptions {
             if (b.id == 200) {
                 b.yPosition = parent.height - 30;
             }
+            if (b.displayString.startswith("Snooper")) {
+                System.out.println(b.xPosition);
+                System.out.println(b.yPosition);
+            }
         });
         buttonList.add(new GuiButton(114514, parent.width / 2 - 155, parent.height / 6 + 18, 150, 20, "Hyperium Settings..."));
     }
