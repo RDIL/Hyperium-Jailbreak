51b29f99c6e74fe113cb7b67326fbd05910f5500
Reece Dunham <me@rdil.rocks>
2019-09-10 18:11:38 -0400

    Merge 2 classes

--- src/main/java/cc/hyperium/mixins/client/renderer/tileentity/MixinRenderItemFrame.java
+++ src/main/java/cc/hyperium/mixins/client/renderer/tileentity/MixinRenderItemFrame.java
@@ -1,6 +1,6 @@
 package cc.hyperium.mixins.client.renderer.tileentity;
 
-import cc.hyperium.mixinsimp.client.renderer.tileentity.HyperiumRenderItemFrame;
+import cc.hyperium.config.Settings;
 import net.minecraft.client.renderer.tileentity.RenderItemFrame;
 import net.minecraft.entity.item.EntityItemFrame;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderItemFrame.class)
 public class MixinRenderItemFrame {
-    private HyperiumRenderItemFrame hyperiumRenderItemFrame = new HyperiumRenderItemFrame();
-
     @Inject(method = "doRender", at = @At("HEAD"), cancellable = true)
     public void doRender(EntityItemFrame entity, double x, double y, double z, float entityYaw, float partialTicks, CallbackInfo ci) {
-        hyperiumRenderItemFrame.doRender(entity, x, y, z, entityYaw, partialTicks, ci);
+        if (Settings.DISABLE_ITEMFRAMES) {
+            ci.cancel();
+        }
     }
 }
