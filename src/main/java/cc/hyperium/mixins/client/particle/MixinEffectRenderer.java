4d5ca27b4b98ab2c3024756690caa5fded540b16
Reece Dunham <me@rdil.rocks>
2019-09-11 21:53:23 -0400

    remove unused import

--- src/main/java/cc/hyperium/mixins/client/particle/MixinEffectRenderer.java
+++ src/main/java/cc/hyperium/mixins/client/particle/MixinEffectRenderer.java
@@ -27,7 +27,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 
