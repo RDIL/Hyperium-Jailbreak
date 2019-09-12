07fbce02680731a1abc2419ab856ac12a5bc26a9
Reece Dunham <me@rdil.rocks>
2019-09-11 22:04:42 -0400

    dont set as cancellable for fields that dont need to be

--- src/main/java/cc/hyperium/mixins/utils/MixinChatComponentStyle.java
+++ src/main/java/cc/hyperium/mixins/utils/MixinChatComponentStyle.java
@@ -5,7 +5,6 @@ import net.minecraft.util.ChatComponentStyle;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.IChatComponent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinChatComponentStyle implements IChatComponent {
     private HyperiumChatComponentStyle hyperiumChatComponentStyle = new HyperiumChatComponentStyle();
 
-    @Inject(method = "setChatStyle", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "setChatStyle", at = @At("HEAD"))
     public void setChatStyle(ChatStyle style, CallbackInfoReturnable<IChatComponent> ci) {
         hyperiumChatComponentStyle.invalidateCache();
     }
@@ -24,7 +23,7 @@ public abstract class MixinChatComponentStyle implements IChatComponent {
         hyperiumChatComponentStyle.getFormatedTextHeader(string);
     }
 
-    @Inject(method = "getFormattedText", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getFormattedText", at = @At("RETURN"))
     public void getFormatedTextReturn(CallbackInfoReturnable<String> string) {
         hyperiumChatComponentStyle.getFormatedTextReturn(string);
     }
