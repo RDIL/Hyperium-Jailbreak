4fad0fc0e4d0e47a865c74e508d10bb080b8bdc5
Reece Dunham <me@rdil.rocks>
2019-09-15 21:59:00 -0400

    merge HyperiumEnchantment into MixinEnchantment

--- src/main/java/cc/hyperium/mixins/MixinEnchantment.java
+++ src/main/java/cc/hyperium/mixins/MixinEnchantment.java
@@ -18,22 +18,20 @@
 package cc.hyperium.mixins;
 
 import cc.hyperium.config.Settings;
-import cc.hyperium.mixinsimp.HyperiumEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.util.StatCollector;
 
 @Mixin(Enchantment.class)
 public abstract class MixinEnchantment {
-    private HyperiumEnchantment enchantment = new HyperiumEnchantment();
-
     @Shadow public abstract String getName();
 
     @Inject(method = "getTranslatedName", at = @At("HEAD"), cancellable = true)
     private void getTranslatedName(int level, CallbackInfoReturnable<String> ci) {
-        if (!Settings.ROMAN_NUMERALS) enchantment.getTranslatedName(level, ci, getName());
+        if (!Settings.ROMAN_NUMERALS) ci.setReturnValue(StatCollector.translateToLocal(name) + " " + level);
     }
 }
