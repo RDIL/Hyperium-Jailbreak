de6f6cbb298bf726895bc8a32f8e95284cd1b104
Reece Dunham <me@rdil.rocks>
2019-09-29 21:55:53 -0400

    Fixes

--- src/main/java/cc/hyperium/handlers/handlers/BroadcastEvents.java
+++ src/main/java/cc/hyperium/handlers/handlers/BroadcastEvents.java
@@ -5,7 +5,6 @@ import cc.hyperium.config.Settings;
 import cc.hyperium.event.AchievementGetEvent;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.InvokeEvent;
-import cc.hyperium.event.LevelupEvent;
 import cc.hyperium.event.ServerChatEvent;
 import me.lpk.util.StringUtils;
 import net.minecraft.util.EnumChatFormatting;
@@ -15,15 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BroadcastEvents {
-    private final Pattern LEVEL_UP_REGEX = Pattern.compile("You are now Hypixel Level (?<level>\\d*)!");
     private final Pattern ACHIEVEMENT_PATTERN = Pattern.compile("a>> {3}Achievement Unlocked: (?<achievement>.+) {3}<<a");
-
-    @InvokeEvent
-    public void levelUp(LevelupEvent event) {
-        if (Settings.BROADCAST_LEVELUPS) {
-            Hyperium.INSTANCE.getHandlers().getCommandQueue().queue("/gchat Levelup! I am now Hypixel Level: " + event.getLevel());
-        }
-    }
 
     @InvokeEvent
     public void acheivementGet(AchievementGetEvent event) {
@@ -35,14 +26,6 @@ public class BroadcastEvents {
     @InvokeEvent
     public void checkForEvents(ServerChatEvent event) {
         String raw = EnumChatFormatting.getTextWithoutFormattingCodes(event.getChat().getUnformattedText());
-        Matcher levelMatcher = LEVEL_UP_REGEX.matcher(raw);
-        if (levelMatcher.matches()) {
-            String level = levelMatcher.group("level");
-            if (StringUtils.isNumeric(level)) {
-                EventBus.INSTANCE.post(new LevelupEvent(Integer.parseInt(level)));
-            }
-        }
-
         Matcher achMatcher = ACHIEVEMENT_PATTERN.matcher(raw);
         if (achMatcher.matches()) {
             String ach = achMatcher.group("achievement");
