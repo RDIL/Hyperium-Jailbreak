9c8c97940ab182a4a770a47e4690a7de3e4e232b
Reece Dunham <me@rdil.rocks>
2019-10-04 08:28:02 -0400

    Add optifine stuff as mixins not asm

---
+++ src/main/java/MixinOptiFinePlugin.java
@@ -0,0 +1,39 @@
+package com.hyperiumjailbreak;
+
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+// makes sure optifine mixins don't load unless optifine is active
+@SupressWarnings("unused")
+public class MixinOptiFinePlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+}
