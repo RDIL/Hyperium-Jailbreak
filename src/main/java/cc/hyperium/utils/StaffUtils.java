62f5e79f19c378acd5beb4ba46516ab1639a27f2
Reece Dunham <me@rdil.rocks>
2019-10-16 12:00:21 -0400

    Fix ref to old class

--- src/main/java/cc/hyperium/utils/StaffUtils.java
+++ src/main/java/cc/hyperium/utils/StaffUtils.java
@@ -2,6 +2,7 @@ package cc.hyperium.utils;
 
 import cc.hyperium.utils.staff.StaffSettings;
 import cc.hyperium.mods.sk1ercommon.Multithreading;
+import com.hyperiumjailbreak.BackendHandler;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -21,7 +22,7 @@ public class StaffUtils {
 
     private static HashMap<UUID, StaffSettings> getStaff() throws IOException {
         HashMap<UUID, StaffSettings> staff = new HashMap<>();
-        String content = InstallerUtils.getRaw("https://raw.githubusercontent.com/hyperiumjailbreak/tools/master/staff.json");
+        String content = BackendHandler.getRawText("https://raw.githubusercontent.com/hyperiumjailbreak/tools/master/staff.json");
         JsonParser parser = new JsonParser();
         JsonArray array = parser.parse(content).getAsJsonArray();
         for (int i = 0; i < array.size(); i++) {
