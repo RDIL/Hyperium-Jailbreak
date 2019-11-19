5e251d934cda51ba06fdd9b456f6b94422a74ce1
Reece Dunham <me@rdil.rocks>
2019-11-19 15:23:18 +0000

    trying

---
+++ src/main/java/com/hyperiumjailbreak/Runtime.java
@@ -0,0 +1,9 @@
+package com.hyperiumjailbreak;
+
+import cc.hyperium.installer.InstallerMain;
+
+public class Runtime {
+    public static void main(String[] args) {
+        InstallerMain.main(args);
+    }
+}
