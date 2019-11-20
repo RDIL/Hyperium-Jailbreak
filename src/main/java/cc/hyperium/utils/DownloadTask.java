073b966ef6786d02fb2a681cd26b005ac34ada4f
Reece Dunham <me@rdil.rocks>
2019-11-20 16:18:17 +0000

    gitpod

---
+++ src/main/java/cc/hyperium/utils/DownloadTask.java
@@ -0,0 +1,74 @@
+/*
+ *     Copyright (C) 2018  Hyperium <https://hyperium.cc/>
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package cc.hyperium.utils;
+
+import javax.swing.SwingWorker;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class DownloadTask extends SwingWorker<Void, Void> {
+    private static final int BUFFER_SIZE = 4096;
+    private final String downloadURL;
+    private final String saveDirectory;
+    private String fileName;
+
+    public DownloadTask(String downloadURL, String saveDirectory) {
+        this.downloadURL = downloadURL.contains("github") ?
+            "https://hyperiumjailbreak.mycloudrepo.io/public/repositories/addons/OptiFine_1.8.9_HD_U_I7.jar"
+            : "https://raw.githubusercontent.com/HyperiumClient/Hyperium-Repo/master/files/mods/OptiFine_1.8.9_HD_U_I7.jar";
+        this.saveDirectory = saveDirectory;
+    }
+
+    @Override
+    protected Void doInBackground() throws Exception {
+        HTTPDownloadUtil util = new HTTPDownloadUtil();
+        util.downloadFile(downloadURL);
+        this.fileName = util.getFileName();
+        File saveFilePath = new File(saveDirectory, util.getFileName());
+
+        InputStream inputStream = util.getInputStream();
+        // opens an output stream to save into file
+        FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead, percentCompleted;
+        long totalBytesRead = 0;
+        long fileSize = util.getContentLength();
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+            totalBytesRead += bytesRead;
+            percentCompleted = (int) (totalBytesRead * 100 / fileSize);
+
+            setProgress(percentCompleted);
+        }
+
+        outputStream.close();
+
+        util.disconnect();
+        return null;
+    }
+
+    @Override
+    protected void done() {}
+
+    public String getFileName() {
+        return fileName;
+    }
+}
