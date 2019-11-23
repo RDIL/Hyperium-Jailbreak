ab4cf84d7f31408c886fdf681de60e5771ac13f1
Reece <Reece>
2019-11-23 15:51:17 -0500

    fun

--- src/main/java/cc/hyperium/mixins/MixinGet.java
+++
@@ -1,7 +0,0 @@
-package cc.hyperium.mixins;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin()
-public class MixinGet {
-}
--- src/main/java/com/hyperiumjailbreak/Runtime.java
+++ src/main/java/com/hyperiumjailbreak/Runtime.java
@@ -1,13 +1,9 @@
 package com.hyperiumjailbreak;
 
 import cc.hyperium.installer.InstallerMain;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.Mixins;
 
 public class Runtime {
     public static void main(String[] args) {
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.hyperium.json");
         InstallerMain.main("local");
     }
 }
