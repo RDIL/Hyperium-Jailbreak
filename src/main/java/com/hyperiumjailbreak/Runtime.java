1cdf139cd5e79bc58be09a1395b58aad13dd7f66
Reece Dunham <me@rdil.rocks>
2020-01-30 19:39:49 +0000

    tips initial

--- src/main/java/com/hyperiumjailbreak/Runtime.java
+++ src/main/java/com/hyperiumjailbreak/Runtime.java
@@ -1,9 +1,27 @@
 package com.hyperiumjailbreak;
 
 import cc.hyperium.installer.InstallerMain;
+import cc.hyperium.mods.sk1ercommon.Multithreading;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Runtime {
     public static void main(String[] args) {
         InstallerMain.main("local");
+    }
+
+    public void scheduleTips() throws InterruptedException {
+        List<Popup> tips = new ArrayList<Popup>();
+        tips.add(new Popup("Visit our website for", "helpful info - hyperiumjailbreak.com"));
+
+        for (Popup tip : tips) {
+            Multithreading.runAsync(() -> {
+                try {
+                    Thread.sleep(new Random().nextLong());
+                    tip.startShowing();
+                } catch (InterruptedException ignored) {}
+            });
+        }
     }
 }
