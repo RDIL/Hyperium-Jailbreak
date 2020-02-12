4b94d6ae3dd6e47d7aa033a998d1a08b3c30a412
Reece Dunham <me@rdil.rocks>
2020-02-12 10:04:54 -0500

    More work

--- src/main/java/com/hyperiumjailbreak/SkyblockAddonsUtils.java
+++ src/main/java/com/hyperiumjailbreak/SkyblockAddonsUtils.java
@@ -2,7 +2,7 @@ package com.hyperiumjailbreak;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
-
+import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.client.TickEvent;
 import net.minecraft.client.Minecraft;
@@ -11,23 +11,15 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.scoreboard.*;
-import java.awt.*;
-import java.util.List;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class SkyblockAddonsUtils {
     public SkyblockAddonsUtils() {
         EventBus.INSTANCE.register(this);
     }
 
-    private final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)§[0-9A-FK-OR]");
-    private static boolean onSkyblock = false;
     private SkyblockAddonsData.Location location = null;
     private int timerTick = 1;
-    private static final Pattern SERVER_REGEX = Pattern.compile("([0-9]{2}/[0-9]{2}/[0-9]{2}) (mini[0-9]{1,3}[A-Za-z])");
 
     public void checkGameLocationDate() {
         boolean foundLocation = false;
@@ -45,14 +37,6 @@ public class SkyblockAddonsUtils {
                 }
                 for (Score score1 : collection) {
                     ScorePlayerTeam scorePlayerTeam = scoreboard.getPlayersTeam(score1.getPlayerName());
-                    String locationString = keepLettersAndNumbersOnly(
-                            stripColor(ScorePlayerTeam.formatPlayerName(scorePlayerTeam, score1.getPlayerName())));
-                    if (locationString.contains("mini")) {
-                        Matcher matcher = SERVER_REGEX.matcher(locationString);
-                        if (matcher.matches()) {
-                            continue; // skip to next line
-                        }
-                    }
                     for (SkyblockAddonsData.Location loopLocation : SkyblockAddonsData.Location.values()) {
                         if (locationString.endsWith(loopLocation.getScoreboardName())) {
                             location = loopLocation;
@@ -68,12 +52,6 @@ public class SkyblockAddonsUtils {
         }
     }
 
-    private static final Pattern LETTERS_NUMBERS = Pattern.compile("[^a-z A-Z:0-9/']");
-
-    private String keepLettersAndNumbersOnly(String text) {
-        return LETTERS_NUMBERS.matcher(text).replaceAll("");
-    }
-
     public boolean isNotNPC(Entity entity) {
         if (entity instanceof EntityOtherPlayerMP) {
             EntityPlayer p = (EntityPlayer)entity;
@@ -87,23 +65,11 @@ public class SkyblockAddonsUtils {
         return true;
     }
 
-    public int getDefaultColor(float alphaFloat) {
-        int alpha = (int) alphaFloat;
-        return new Color(150, 236, 255, alpha).getRGB();
-    }
-
-    public String stripColor(final String input) {
-        return STRIP_COLOR_PATTERN.matcher(input).replaceAll("");
-    }
-
     public SkyblockAddonsData.Location getLocation() {
         return location;
     }
 
-    public boolean isOnSkyblock() {
-        return onSkyblock;
-    }
-
+    @InvokeEvent
     public void onTick(TickEvent e) {
         timerTick++;
         Minecraft mc = Minecraft.getMinecraft();
