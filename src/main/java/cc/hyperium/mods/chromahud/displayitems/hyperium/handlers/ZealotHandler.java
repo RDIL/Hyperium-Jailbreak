68fdf49e1f5163275ee782ba4517294b513b3376
Reece Dunham <me@rdil.rocks>
2020-08-15 10:10:02 +0000

    Use native zealot checking

--- src/main/java/cc/hyperium/mods/chromahud/displayitems/hyperium/handlers/ZealotHandler.java
+++ src/main/java/cc/hyperium/mods/chromahud/displayitems/hyperium/handlers/ZealotHandler.java
@@ -1,9 +1,14 @@
 package cc.hyperium.mods.chromahud.displayitems.hyperium.handlers;
 
+import java.util.List;
+
 import cc.hyperium.Hyperium;
 import cc.hyperium.event.InvokeEvent;
 import cc.hyperium.event.entity.PlayerAttackEntityEvent;
-import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.util.AxisAlignedBB;
 
 public class ZealotHandler {
     public static ZealotHandler INSTANCE = new ZealotHandler();
@@ -12,12 +17,18 @@ public class ZealotHandler {
     @InvokeEvent
     public void onEntityKilled(PlayerAttackEntityEvent event) {
         if (event.getEntity().isDead && Hyperium.INSTANCE.getHandlers().getHypixelDetector().isHypixel()) {
-            if (event.getEntity() instanceof EntityEnderman) {
-                if (event.getEntity().getName().toLowerCase().equals("zealot")) {
-                    slainZealots++;
-                }
+            if (isZealot(event.getEntity())) {
+                slainZealots++;
             }
         }
+    }
+
+    public boolean isZealot(Entity enderman) {
+        List<EntityArmorStand> stands = Minecraft.getMinecraft().theWorld.getEntitiesWithinAABB(EntityArmorStand.class, new AxisAlignedBB(enderman.posX - 1, enderman.posY, enderman.posZ - 1, enderman.posX + 1, enderman.posY + 5, enderman.posZ + 1));
+        if (stands.isEmpty()) return false;
+
+        EntityArmorStand armorStand = stands.get(0);
+        return armorStand.hasCustomName() && armorStand.getCustomNameTag().contains("Zealot");
     }
 
     public int getSlainZealots() {
