160f845c126bfbf58ede80141ed2c92991cee9d6
Reece <me@rdil.rocks>
2020-11-21 10:32:59 -0500

    Revert one change

--- src/main/java/cc/hyperium/mixins/MixinChunkProviderClient.java
+++ src/main/java/cc/hyperium/mixins/MixinChunkProviderClient.java
@@ -1,18 +1,33 @@
 package cc.hyperium.mixins;
 
+import net.minecraft.util.LongHashMap;
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 
 import cc.hyperium.event.EventBus;
 import cc.hyperium.event.world.ChunkLoadEvent;
 import net.minecraft.client.multiplayer.ChunkProviderClient;
 import net.minecraft.world.chunk.Chunk;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
 
 @Mixin(ChunkProviderClient.class)
 public class MixinChunkProviderClient {
-    @ModifyArg(method = "loadChunk", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), index = 1)
-    public Chunk chunkListing$add(Chunk chunk) {
+    @Shadow private World worldObj;
+    @Shadow private LongHashMap<Chunk> chunkMapping;
+    @Shadow private List<Chunk> chunkListing;
+
+    /**
+     * @author Reece Dunham and Mojang
+     */
+    @Overwrite
+    public Chunk loadChunk(int p_73158_1_, int p_73158_2_) {
+        Chunk chunk = new Chunk(this.worldObj, p_73158_1_, p_73158_2_);
+        this.chunkMapping.add(ChunkCoordIntPair.chunkXZ2Int(p_73158_1_, p_73158_2_), chunk);
+        this.chunkListing.add(chunk);
         EventBus.INSTANCE.post(new ChunkLoadEvent(chunk));
         return chunk;
     }
