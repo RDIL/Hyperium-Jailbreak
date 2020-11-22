aac94bf93102c9b13a4c1a2776847433f4e38bb6
Reece <me@rdil.rocks>
2020-11-22 13:18:32 -0500

    Revert a few things

--- src/main/java/cc/hyperium/event/EventSubscriber.java
+++ src/main/java/cc/hyperium/event/EventSubscriber.java
@@ -18,14 +18,18 @@
 package cc.hyperium.event;
 
 import com.google.common.base.Preconditions;
+import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.Method;
 
 public final class EventSubscriber {
+    @NotNull
     private final Object instance;
+    @NotNull
     private final Method method;
+    @NotNull
     private final Priority priority;
 
-    EventSubscriber(Object instance, Method method, Priority priority) {
+    EventSubscriber(@NotNull Object instance, @NotNull Method method, @NotNull Priority priority) {
         Preconditions.checkNotNull(instance, "instance cannot be null");
         Preconditions.checkNotNull(method, "method cannot be null");
         Preconditions.checkNotNull(priority, "priority cannot be null");
@@ -35,19 +39,23 @@ public final class EventSubscriber {
         this.priority = priority;
     }
 
+    @NotNull
     public final Object getInstance() {
         return this.instance;
     }
 
+    @NotNull
     public final Method getMethod() {
         return this.method;
     }
 
+    @NotNull
     final Priority getPriority() {
         return this.priority;
     }
 
-    public final EventSubscriber copy(Object instance, Method method, Priority priority) {
+    @NotNull
+    public final EventSubscriber copy(@NotNull Object instance, @NotNull Method method, @NotNull Priority priority) {
         Preconditions.checkNotNull(instance, "instance");
         Preconditions.checkNotNull(method, "method");
         Preconditions.checkNotNull(priority, "priority");
@@ -72,8 +80,8 @@ public final class EventSubscriber {
                 EventSubscriber eventSubscriber = (EventSubscriber) subscriberIn;
 
                 return this.instance.equals(eventSubscriber.instance) &&
-                    this.method.equals(eventSubscriber.method) &&
-                    this.priority.equals(eventSubscriber.priority);
+                        this.method.equals(eventSubscriber.method) &&
+                        this.priority.equals(eventSubscriber.priority);
             }
 
             return false;
